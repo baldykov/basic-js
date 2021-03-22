e8beb2c185b394cc5ca0cd2fccdb9003ab49e423
Дмитрий Балдыков <baldykovdima@yandex.ru>
2021-03-22 22:19:18 +0300

    feat: dream-team

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,11 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if (!members || typeof members !== "object" || !members.length) return false;
+  return members
+    .map((name) =>
+      typeof name === "string" && name.length > 0
+        ? name.trim()[0].toUpperCase()
+        : ""
+    )
+    .sort()
+    .join("");
 };
