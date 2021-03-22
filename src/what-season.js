20287ed76ee17ba52e80a87a048fcf75b7311992
Дмитрий Балдыков <baldykovdima@yandex.ru>
2021-03-22 22:44:44 +0300

    feat: what-season

--- src/what-season.js
+++ src/what-season.js
@@ -1,6 +1,20 @@
-const CustomError = require("../extensions/custom-error");
-
-module.exports = function getSeason(/* date */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function getSeason(date) {
+  if (!date) return "Unable to determine the time of year!";
+  try {
+    const month = date.toJSON().split("-")[1];
+    switch (Math.floor(month / 3)) {
+      case 0:
+        return "winter";
+      case 1:
+        return "spring";
+      case 2:
+        return "summer";
+      case 3:
+        return "autumn";
+      case 4:
+        return "winter";
+    }
+  } catch {
+    throw "Some Error";
+  }
 };
