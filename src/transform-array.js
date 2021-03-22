6acd9ce9adf94b4ae2a4a3bc2d7a153cefeb638a
Дмитрий Балдыков <baldykovdima@yandex.ru>
2021-03-22 23:57:44 +0300

    feat: transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,31 @@
-const CustomError = require("../extensions/custom-error");
+module.exports = function transform(arr) {
+  console.log(arr);
+  const result = [...arr];
+  result.forEach((e, i) => {
+    switch (e) {
+      case "--discard-next":
+        if (result[i + 1] !== undefined) result[i + 1] = "discard";
+        result.splice(i, 1);
+        break;
+      case "--discard-prev":
+        if (result[i - 1] !== undefined) result[i - 1] = "discard";
+        result.splice(i, 1);
+        break;
+      case "--double-next":
+        if (result[i + 1] !== undefined) result.splice(i, 1, result[i + 1]);
+        else result.splice(i, 1);
+        break;
+      case "--double-prev":
+        if (result[i - 1] !== undefined) result.splice(i, 1, result[i - 1]);
+        else result.splice(i, 1);
+        break;
+    }
+  });
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+  return result.filter((item) => item != "discard");
 };
+
+// --discard-next excludes the next element of the array from the transformed array.
+// --discard-prev excludes the previous element of the array from the transformed array.
+// --double-next doubles the next element of the array in the transformed array.
+// --double-prev doubles the previous element of the array in the transformed array.
